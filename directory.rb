4ef70c3afd81cf21acdb45dc8450fcaa7a4f6ae1
arhussain1 <a.r.hussain@hotmail.com>
2022-02-14 21:05:11 +0000

    Added additional comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Lets begin by printing the list of student we have
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,9 @@ puts "Freddy Kruegar"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# At the end we can add the total number of students
 print "Overall, we have "
+# Note: we used the print function as opposed to the puts
+# this is because print doesn't add a new line "\n" at the end
 print 11
 puts " great students"
