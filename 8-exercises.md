5af9cf9fc5f33f1edc96ddb4b1ae9f9f31ea724b
arhussain1 <a.r.hussain@hotmail.com>
2022-02-15 11:03:19 +0000

    Numbered students using each_with_index method
    Updated task list

--- 8-exercises.md
+++ 8-exercises.md
@@ -3,4 +3,4 @@ Below are the specifications for each task, check them off as you go through the
 
 | Status | Task | Description |
 | --- | --- | --- |
-| [x] | **1** | We're using the each() method to iterate over an array of students. How can you modify the program to print a number before the name of each student, e.g. "1. Dr. Hannibal Lecter"? Hint: look into `each_with_index()` | 
+| :white_check_mark: | **1** | We're using the each() method to iterate over an array of students. How can you modify the program to print a number before the name of each student, e.g. "1. Dr. Hannibal Lecter"? Hint: look into `each_with_index()` | 
---
+++ 8-exercises/8-exercises.rb
@@ -0,0 +1,50 @@
+
+# Add user input
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # use user input to get the first name
+  name = gets.chomp
+  
+  while !name.empty? do
+    # add the student given to us as a hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get the next student
+    name = gets.chomp
+  end
+  # return the array of students once the person is done
+  students
+end
+
+# Create the default header method
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------------"
+end
+
+# Next lets create the print method with 1 argument 'names'
+def print(students)
+  students.each_with_index do |student, number|
+    puts "#{number + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+# Finally lets create the print_footer method
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# Lets begin by getting the users input for the hash of student
+# then save it to the variable students
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
+# Notice how much easier the last few lines are to read
+# it's almost like english
+#
+# Even though we used more lines of code this is a better solution
