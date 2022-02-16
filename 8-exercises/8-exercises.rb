b05ca70a151e63d38012beb3a3fa43599db9dc82
arhussain1 <a.r.hussain@hotmail.com>
2022-02-16 16:50:53 +0000

    Completed Step8-Task7 and refactored solution

--- 8-exercises/8-exercises.rb
+++ 8-exercises/8-exercises.rb
@@ -1,25 +1,42 @@
-
-# Add user input
 def input_students
+  puts "Please enter the information for the students"
+  puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # Create the name variable and put something in it to allow the while loop to run
-  name = "foo"
-  while !name.empty? do
-    # Ask for each variable 
-    puts "Please enter the name of the student"
-    puts "To finish, just hit return twice"
+  
+  # Create an array to check for typos for cohort months
+  typo_check = ["No entry", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  while true do
+    puts "Name: "
     name = gets.chomp
-    break if name.empty?
+    puts "Cohort: "
+    cohort = gets.chomp
+
+    # Create a break for no entries situation
+    if name.empty? && cohort.empty?
+      break
+    elsif name.empty?
+      name = "No entry"
+    elsif cohort.empty?
+      cohort = "No entry"
+    end
+
+    # Check for typos for cohort variable
+    while !typo_check.include?(cohort.downcase.capitalize)
+      puts "Enter Cohort again or input 'No entry': "
+      cohort = gets.chomp
+    end
+    
     # add the student given to us as a hash to the array
-    students << {name: name.capitalize, cohort: :november}
+    students << {name: name.downcase.capitalize, cohort: cohort.downcase.capitalize.to_sym}
     puts "Now we have #{students.count} students"
   end
   # return the array of students once the person is done
   students
 end
 
-# Create the default header method
+# This represents the default header that will always
 def print_header
   puts "The students of Villains Academy"
   puts "-------------------"
@@ -27,16 +44,15 @@ end
 
 # Next lets create the print method with 1 argument 'names'
 def print(students)
-  students.each_with_index do |student, number|  
-    output = "#{number + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
-    puts output.center(40)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 # Finally lets create the print_footer method
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end  
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
 # Lets begin by getting the users input for the hash of student
 # then save it to the variable students
