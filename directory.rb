5eba280a226d0d16420dadbe3c58a80dc31d11df
arhussain1 <a.r.hussain@hotmail.com>
2022-02-17 16:50:40 +0000

    Refactored interactive_menu method

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
+@students = [] # This is a global variable acessible to all methods
+
 # This is a complete do over of the student directory from scratch with a 
 # interactive menu
 
 def input_students
   puts "Please enter the information for the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
 
   # Create an array to check for typos for cohort months
   typo_check = ["No entry", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -35,11 +35,9 @@ def input_students
     name_formatted = name.downcase.split(" ").each{|word| word.capitalize!}.join(" ")
 
     # add the student given to us as a hash to the array
-    students << {name: name_formatted, cohort: cohort.downcase.capitalize.to_sym}
-    puts "Now we have #{students.count} students"
+    @students << {name: name_formatted, cohort: cohort.downcase.capitalize.to_sym}
+    puts "Now we have #{@students.count} students"
   end
-  # return the array of students once the person is done
-  students
 end
 
 # This represents the default header that will always
@@ -49,12 +47,12 @@ def print_header
 end
 
 # Next lets create the print method with 1 argument 'names'
-def print(students)
+def print_students_list
 
   # organise everyone by their cohort and store them in sorted_by_cohort
   sorted_by_cohort = {}
 
-  students.each do |student|
+  @students.each do |student|
     cohort = student[:cohort]
 
     if sorted_by_cohort[cohort] == nil
@@ -92,45 +90,53 @@ def print(students)
 end
 
 # Finally lets create the print_footer method
-def print_footer(names)
+def print_footer
   # create a placeholder
   placeholder =
   # figure out whether we should use plural or singular
-  names.count > 1 ? placeholder = "students" : placeholder = "student"
-  puts "Overall, we have #{names.count} great #{placeholder}"
+  @students.count > 1 ? placeholder = "students" : placeholder = "student"
+  puts "Overall, we have #{@students.count} great #{placeholder}"
 end
 
+def print_menu
+  # 1. print the menu i.e. the list of options and ask the user what to do
+  puts "Choose from the options below:"
+  puts "1. Input a new student"
+  puts "2. See current students"
+  puts "9. Exit"
+end
+
+def show_students
+  # Show current list of students"
+  if @students.length > 0 
+    print_students_list
+    print_footer
+  else
+    puts "There are no students to show"
+  end
+end
+
+def process(selection)
+  # 3. implement what user wants
+  case selection
+  when "1"
+    # Add the students
+    input_students
+  when "2"
+    show_students
+  when "9"
+    # Exit the program
+    exit
+  else
+    puts "Please try another option"
+  end
+end
 
 def interactive_menu
-  students = []
   # Create a repeating loop 
   loop do
-    # 1. print the menu i.e. the list of options and ask the user what to do
-    puts "Choose from the options below:"
-    puts "1. Input a new student"
-    puts "2. See current students"
-    puts "9. Exit"
-    # 2. save that user input
-    selection = gets.chomp
-    # 3. implement what user wants
-    case selection
-      when "1"
-        # Add the students
-        students = input_students
-      when "2"
-        # Show current list of students"
-        if students.length > 0 
-          print(students)
-          print_footer(students)
-        else
-          puts "There are no students to show"
-        end
-      when "9"
-        # Exit the program
-        exit
-      else
-        puts "Please try another option"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
